e9ad039f1248773d53ddaec24511da891eb0c70d
Joshua Horacsek <joshua@Macintosh.local>
2015-01-03 13:19:59 -0800

    Adding project structure, some setup code, and basic msp init code.

--- msptree/msptree.py
+++ msptree/msptree.py
@@ -1,53 +1,76 @@
+#!/usr/bin/env python
+""" Multi-resolution Sphere Packing tree
+
+This file contains an implementation of
+    Multi-resolution sphere packing tree: a hierarchical multi-resolution 3D data structure
+        Jiro Inoue          Queen's University, Ontario, Canada
+        A. James Stewart    Queen's University, Ontario, Canada
+
+    @author Joshua Horacsek
 """
-"""
+
+
 class MSPTreeType:
-	"""
-	"""
-	FunctionSpace, SpacePartitioning = range(2)
+    """ Defines how the MSP will be used, a function space has a coefficient at each level of the tree for use with
+    a hierarchical reconstruction scheme. A space partitioning tree just partitions space so only the last node contains
+    a bin in which a value is stored.
+    """
+    FunctionSpace, SpacePartitioning = range(2)
+
+    def __init__(self):
+        pass
 
 
 class MSPTree(object):
-	"""
-	"""
-	class _NodeType:
-    	CC_Node, BCC_Node, FCC_Node = range(3)
+    """ MSP Tree Implementation
+    """
+
+    class _NodeType:
+        CC_Node, BCC_Node, FCC_Node = range(3)
+
+        def __init__(self):
+            pass
 
     class _NodeColor:
-    	Node_Red, Node_Green, Node_Blue, Node_Yellow, Node_Ghost = range(5)
+        Node_Red, Node_Green, Node_Blue, Node_Yellow, Node_Ghost = range(5)
 
-	class _Node:
-		def __init__(self, position, color,  value = None):
-			"""
-			"""
-			self.value = value
-			self.position = position
-			self.expanded = False
-			self.color = color
-			self.lattice = None
+        def __init__(self):
+            pass
 
-		def expand_node(self, depth, scale):
-			"""
-			"""
-			pass
+    class _Node:
 
-		def expand_cc_node(self, depth, scale):
-			"""
-			"""
-			pass
+        def __init__(self, position, color,  value=None):
+            """
+            """
+            self.value = value
+            self.position = position
+            self.expanded = False
+            self.color = color
+            self.lattice = None
 
-		def expand_bcc_node(self, depth, scale):
-			"""
-			"""
-			pass
+        def expand_node(self, depth, scale):
+            """
+            """
+            pass
 
-	def __init__(self, max_depth, tree_type=MSPTreeType.FunctionSpace):
-		"""
-		"""
-		self.roots = [];
+        def expand_cc_node(self, depth, scale):
+            """
+            """
+            pass
 
-	def find_closest_node(point, level=-1):
-		pass
+        def expand_bcc_node(self, depth, scale):
+            """
+            """
+            pass
 
-	def expand_to(point):
-		pass
+    def __init__(self, max_depth, tree_type=MSPTreeType.FunctionSpace):
+        """
+        """
+        self.roots = [];
+
+    def find_closest_node(point, level=-1):
+        pass
+
+    def expand_to(point):
+        pass
 
---
+++ setup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+
+from setuptools import setup, find_packages
+
+setup(name='Multi-resolution Sphere Packing Tree',
+      version='0.1',
+      description='Python Distribution Utilities',
+      author='Joshua Horacsek',
+#      author_email='',
+#      url='https://www.python.org/sigs/distutils-sig/',
+      packages=['msptree', 'msptree.msptree'],
+     )
